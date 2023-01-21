a6c93094a6c11dec1b840420ed9e5995199837da
MEHDI BELLAM <porcepic91@gmail.com>
2023-01-21 13:17:24 +0100

    ajout des demandes de l'énonce dans la page présentation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>|
+
+    <router-link to="/about">présenation</router-link>|
     <router-link to="/Journal">Journal</router-link> |
     <router-link to="/contact">contact</router-link> |
     <router-link to="/clavier">clavier</router-link> |
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Projet Vue Js</h1>
+
+    <span>
+      Ceci est l'un de mes projets en vue JS.
+      l'objectif est de créer une app de contact
+      voici la liste des fonctionnalité demandé
+
+    </span>
+    <ul>
+      <li>Un clavier numérique </li>
+      <li>Un journal d'appels</li>
+      <li> Une liste de contact</li>
+      <li> lorsque l'on  compose un numéro en appuyant sur les touches .
+      </li>
+      <li> Le numéro s'affiche en temps réel</li>
+      <li>Afficher le nom du contact s'il est dans notre liste de contacte </li>
+      <li>lorsque l'on appuie sur le bouton
+        « appeler», une nouvelle entrée apparaît dans le journal .
+      </li>
+      <li> afficher  le numéro (ou le nom si dans contact) et la date et heure du call  dans le journal.</li>
+      <li> Dans l'onglet contact on peux appeler directement en cliquant sur l'icone </li>
+      <li> Créer un formulaire pour ajouter un contact ( sur une autre page )</li>
+
+    </ul>
   </div>
 </template>
