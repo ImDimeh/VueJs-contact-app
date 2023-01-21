5214110b54074b35d17412b31d1a757e5a904cd7
MEHDI BELLAM <porcepic91@gmail.com>
2023-01-21 13:26:30 +0100

    verification de la liste des fonctionnalité

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,26 +3,33 @@
     <h1>Projet Vue Js</h1>
 
     <span>
-      Ceci est l'un de mes projets en vue JS.
-      l'objectif est de créer une app de contact
-      voici la liste des fonctionnalité demandé
-
+      Ceci est l'un de mes projets en vue JS. L'objectif est de créer une application de contact. Voici la liste des fonctionnalités demandées :
     </span>
     <ul>
-      <li>Un clavier numérique </li>
+      <li>Un clavier numérique,</li>
       <li>Un journal d'appels</li>
-      <li> Une liste de contact</li>
-      <li> lorsque l'on  compose un numéro en appuyant sur les touches .
+      <li>Une liste de contacts.</li>
+      <li>  Lorsque l'on compose un numéro en appuyant sur les touches,
+
       </li>
-      <li> Le numéro s'affiche en temps réel</li>
-      <li>Afficher le nom du contact s'il est dans notre liste de contacte </li>
-      <li>lorsque l'on appuie sur le bouton
-        « appeler», une nouvelle entrée apparaît dans le journal .
+      <li> Le numéro s'affiche en temps réel.</li>
+      <li>Afficher le nom du contact s'il est dans notre liste de contacts.</li>
+      <li>Lorsque l'on appuie sur le bouton "appeler", une nouvelle entrée apparaît dans le journal.
       </li>
-      <li> afficher  le numéro (ou le nom si dans contact) et la date et heure du call  dans le journal.</li>
-      <li> Dans l'onglet contact on peux appeler directement en cliquant sur l'icone </li>
-      <li> Créer un formulaire pour ajouter un contact ( sur une autre page )</li>
+      <li> Afficher le numéro (ou le nom si dans contact) et la date et l'heure du call dans le journal.</li>
+      <li> Dans l'onglet contact, on peut appeler directement en cliquant sur l'icône.</li>
+      <li>Créer un formulaire pour ajouter un contact (sur une autre page).</li>
 
     </ul>
   </div>
 </template>
+<style>
+ul {
+
+}
+
+li {
+  list-style: none;
+}
+</style>
+
